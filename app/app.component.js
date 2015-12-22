703d36486e62b7ffd9a7a3784ba8867be935573f
juno <juno@krasava.fi>
2015-12-22 07:22:01 +0200

    dev

--- app/app.component.js
+++ app/app.component.js
@@ -18,11 +18,17 @@ System.register(['angular2/core'], function(exports_1) {
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.title = 'Tour of Heroes';
+                    this.hero = 'Windstorm';
+                    this.hero = {
+                        id: 1,
+                        name: 'Windstorm'
+                    };
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: "\n  <h1>{{title}}</h1>\n  <h2>{{hero.name}} details!</h2>\n  <div><label>id: </label>{{hero.id}}</div>\n  <div>\n    <label>name: </label>\n    <div><input value=\"{{hero.name}}\" placeholder=\"name\"></div>\n  </div>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
