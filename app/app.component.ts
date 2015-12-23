2dc653fd539793a0668aa3580e03dd2317c77a09
juno <juno@krasava.fi>
2015-12-23 07:43:31 +0200

    dev

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,75 +1,51 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
-
-interface Hero {
-  id: number;
-  name: string;
-}
-
-
+import {HeroService} from './hero.service';
 @Component({
-    selector: 'my-app',
-    
-    template:`
-  <h1>{{title}}</h1>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="#hero of heroes"
-      [class.selected]="hero === selectedHero"
-      (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  selector: 'my-app',
+  template:`
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
-    
-    styles:[`
-  .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
-  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
-  .heroes .badge {
-    font-size: small;
-    color: white;
-    padding: 0.1em 0.7em;
-    background-color: #369;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -1px;
-  }
-  .selected { background-color: #EEE; color: #369; }
-`],
-directives: [HeroDetailComponent]
+  styles:[`
+    .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+    .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+    .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+    .heroes .badge {
+      font-size: small;
+      color: white;
+      padding: 0.1em 0.7em;
+      background-color: #369;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -1px;
+    }
+    .selected { background-color: #EEE; color: #369; }
+  `],
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
- 
-   
-export class AppComponent {
-        
+export class AppComponent implements OnInit {
   public title = 'Tour of Heroes';
-    
-   public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-    }; 
- 
-    public selectedHero: Hero;
-    
-    public heroes = HEROES;
-    
-        
-    onSelect(hero: Hero) { this.selectedHero = hero; }
-
+  public heroes: Hero[];
+  public selectedHero: Hero;
+  constructor(private _heroService: HeroService) { }
+  getHeroes() {
+    this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
-   var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
